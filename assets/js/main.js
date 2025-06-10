de4b23e90d6fbba5b97190bbaa7c649583205948
fabian nugrah <fabianugerah@gmail.com>
2025-06-10 09:31:28 +0700

    New Portofolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 (function() {
   "use strict";
 
-  /**
-   * Apply .scrolled class to the body as the page is scrolled down
-   */
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
@@ -14,3 +11,44 @@
   document.addEventListener('scroll', toggleScrolled);
   window.addEventListener('load', toggleScrolled);
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const _swiper = new Swiper('.mySwiper', {
+        loop: true,
+        slidesPerView: 5,
+        spaceBetween: 30,
+
+
+        autoplay: {
+            delay: 500, // Coba kurangi delay menjadi 0.5 detik (500ms) untuk transisi lebih cepat/responsif
+            disableOnInteraction: false,
+        },
+        speed: 800, // Coba kurangi speed menjadi 0.8 detik (800ms) untuk transisi lebih cepat/responsif
+
+        grabCursor: true,
+        freeMode: false,
+
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 25
+            },
+            992: {
+                slidesPerView: 5,
+                spaceBetween: 30
+            },
+            1200: {
+                slidesPerView: 6,
+                spaceBetween: 40
+            }
+        }
+    });
+});
